1e495713599408b17a26e2a9b4a5febb14895628
liqijunchn <liqijunchn@163.com>
2017-11-25 21:06:50 +0800

    鎶妕extfield鍜孴extarea鏀逛负鎴愬憳鍙橀噺

--- chat/src/ChatClient.java
+++ chat/src/ChatClient.java
@@ -2,17 +2,18 @@ import java.awt.*;
 
 public class ChatClient extends Frame {
 	
+	TextArea ta = new TextArea(4,40);
+	TextField tf = new TextField();	
+	
 	public static void main(String[] args) {
 		new ChatClient().launchFrame();
-		
+
 
 	}
 	
 	public void launchFrame() {
 		setLocation(400,400);
 		setSize(300,300);
-		TextArea ta = new TextArea(4,40);
-		TextField tf = new TextField();
 		add(ta,BorderLayout.NORTH);
 		add(tf,BorderLayout.CENTER);
 		setVisible(true);
