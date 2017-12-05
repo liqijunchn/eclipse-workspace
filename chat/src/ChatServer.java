182221dfdfedbe10aeef4756cb5cfcf38d1c98fb
liqijunchn <liqijunchn@163.com>
2017-12-05 22:09:22 +0800

    add multiple thread

--- chat/src/ChatServer.java
+++ chat/src/ChatServer.java
@@ -4,25 +4,80 @@ import java.io.*;
 
 public class ChatServer {
 	
+	ServerSocket ss = null;
+	boolean serverStarted = false;
+	
 	public static void main(String[] args) {
+		new ChatServer().start();
+
+	}
+	
+	public void start() {
 		try {
-			boolean serverStarted = false;
-			ServerSocket ss = new ServerSocket(8888);
+			ss = new ServerSocket(8888);
 			serverStarted = true;
-			while(serverStarted) {
-				boolean bconnect = false;
-				Socket s = ss.accept();
-				bconnect = true;
-				DataInputStream dis = new DataInputStream(s.getInputStream());
-				while(bconnect) {
-					String str = dis.readUTF();
-					System.out.println(str);
-			}
-				dis.close();
-			}
-		} catch (IOException e) {
+		} catch(BindException e) {
+			System.out.println("端口占用中...");
+			System.exit(0);
+		} catch(IOException e) {
 			e.printStackTrace();
+		}
+		try {
+			while(serverStarted) {
+
+				Socket s = ss.accept();
+				Client c = new Client(s);
+				new Thread(c).start();
+			}
+		} catch (EOFException e) {
+			System.out.println("client close");
+		} catch(IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				ss.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+			
 		}	
 	}
 
-}
+	class Client implements Runnable {
+		Socket s = null;
+		DataInputStream dis = null;
+		boolean bconnect = false;
+		
+		public Client(Socket s) {
+			this.s = s;
+			
+		}
+		@Override
+		public void run() {	
+			try {
+				dis = new DataInputStream(s.getInputStream());
+				bconnect = true;
+				while(bconnect) {
+					String str = dis.readUTF();
+					System.out.println(str);
+				}				
+			} catch(EOFException e){
+				System.out.println("Close Client");
+				System.exit(0);
+			} catch (IOException e) {
+				e.printStackTrace();
+				
+			} finally {
+				try {
+					dis.close();
+					s.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		
+			}
+		
+		
+	}
+}	
